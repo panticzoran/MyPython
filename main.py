b993e64242626057f61c138a798b2366215675cf
Zoran Pantić <91920971+panticzoran@users.noreply.github.com>
2021-11-06 21:39:00 +0000

    Adding factorial and quadratic equation

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-def factorial(n):
-  if n == 0:
-    return 1
-  else:
-    return n * factorial(n - 1)
-print(factorial(5))
+
+
+print("Hello World")
+print(2+3)
+
+from quadratic_equation import solve_quadratic
+answer = solve_quadratic(5, 6, 1)
+print(answer)
+
+from factorial import factorial
+print("Factorial 5 = " + str(factorial(5)))
