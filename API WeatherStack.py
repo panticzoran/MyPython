c2e34155567bb521b3d4cb292d024c6459fc4572
Zoran Pantić <91920971+panticzoran@users.noreply.github.com>
2021-12-05 20:34:41 +0000

    Added reading data from API and putting them into separate files

---
+++ API WeatherStack.py
@@ -0,0 +1,28 @@
+# Testing API with WeatherStack
+
+import os
+import requests
+
+# Local ReplIt: hiding my WeatherStack API key
+my_secret = os.environ['APIkeyWeatherStack']
+
+# Defining access arameters
+API_KEY = my_secret
+WS_URL = "http://api.weatherstack.com/current"
+city = "Belgrade"
+accessParameters = {'access_key': API_KEY, 'query': city}
+
+response = requests.get(WS_URL, accessParameters)
+js = response.json()
+
+# Printing out raw JSON data
+print(js)
+print()
+
+# Printing out formatted data
+temperature = js['current']['temperature']
+date = js['location']['localtime']
+city = js['location']['name']
+country = js['location']['country']
+feelsLike = js['current']['feelslike']
+print(f"The temperature in {city}, {country} on {date} is {temperature} degrees Celsius, which feels like {feelsLike} degrees Celsius")
--- main.py
+++ main.py
@@ -1,19 +1,34 @@
+# Testing API with WeatherStack
+
 import os
 import requests
-# change the following line to use your own API key
+
+# Local ReplIt: hiding my WeatherStack API key
 my_secret = os.environ['APIkeyWeatherStack']
+
+# Defining access arameters
 API_KEY = my_secret
 WS_URL = "http://api.weatherstack.com/current"
-city = "Belgrade"
-parameters = {'access_key': API_KEY, 'query': city}
-response = requests.get(WS_URL, parameters)
-js = response.json()
 
-print(js)
-print()
+# Read the city names from txt file and put each city into a Python list
+cities = []
+with open("WS_cities.txt") as f:
+	for line in f:
+		cities.append(line.strip())
+# Prints the Python list
+print(cities)
 
-temperature = js['current']['temperature']
-date = js['location']['localtime']
-city = js['location']['name']
-country = js['location']['country']
-print(f"The temperature in {city}, {country} on {date} is {temperature} degrees Celsius")
+
+# Loops through the cities
+for city in cities:
+	
+	# Gets the weather data for each one
+	parameters = {'access_key': API_KEY, 'query': city}
+	response = requests.get(WS_URL, parameters)
+	js = response.json()
+	temperature = js['current']['temperature']
+	date = js['location']['localtime']
+	
+	# Opens a file with the same name as each city and writes the date and temperature to each file
+	with open(f"WS_{city}.txt", "a") as f:
+		f.write(f"{date},{temperature}\n")
