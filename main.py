14f9a2f1ccfa91b29962a6604c48479809c4cbd8
panticzoran <91920971+panticzoran@users.noreply.github.com>
2023-11-28 20:30:40 +0000

    Working version of checking if a number is perfect

--- main.py
+++ main.py
@@ -1,48 +1,22 @@
-# In array of N elements find the biggest subset of repeating numbers, and 
-# print what numbert it is, what is it's position and how big is the subset
+# Perfect number, a positive integer that is equal to the sum of its proper divisors
+# The smallest perfect number is 6, which is the sum of 1, 2, and 3. 
+# Other perfect numbers are 28, 496, and 8128.
+
+def isProperDivisor(nr , divisor):
+  ''' Returns True if proper divisor, otherwise False'''
+  return nr % divisor == 0
+
+def isPerfect(nr):
+  '''Returns True if perfect number, otherwise False'''
+  sumDivisors = 1
+  for i in range (2, nr):
+    if isProperDivisor(nr,i):
+      sumDivisors += i
+  return sumDivisors == nr    
 
 
-import random
+checkUntil = 100000 # Check for perfect numbers from 1 to checkUntil
 
-nrElements = 40
-
-print("The number of elements in the array: ", nrElements)
-print("\n")
-
-print("The elements of the array are: ")
-print("\n")
-
-element = random.randint(1, 5) # Generates random integer 1 - 5 (inclusive)
-previousEl = element
-
-print("1: " + str(element))
-
-theElement = element
-theFinalElement = element
-
-index1stElement = 1
-theIndex1stElement = 1
-
-biggestSize = 1
-theBiggestSize = 1
-
-
-for i in range (2,nrElements):
-  previousEl = element
-  element = random.randint(1, 5)
-  print(str(i) + ": " + str(element))
-  
-  if element == previousEl:
-    theElement = element
-    biggestSize = biggestSize + 1
-    index1stElement = i - biggestSize + 1
-  else:
-    biggestSize = 1
-
-  if biggestSize >= theBiggestSize:
-    theFinalElement = previousEl
-    theBiggestSize = biggestSize
-    theIndex1stElement = index1stElement
-
-print("The biggest subset is having", theBiggestSize ,  "elements '" + 
-      str(theFinalElement) + "' starting at the position" , theIndex1stElement)
+for i in range(2, checkUntil):
+  if isPerfect(i):
+    print("The number", i , " is perfect number")
