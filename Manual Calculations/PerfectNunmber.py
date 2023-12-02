380c7f694a0d24779ec864e739747701dfe6a4a0
panticzoran <91920971+panticzoran@users.noreply.github.com>
2023-12-02 21:20:01 +0000

    Preparing the folder structure for Manual Calculations

---
+++ Manual Calculations/PerfectNunmber.py
@@ -0,0 +1,23 @@
+# Perfect number, a positive integer that is equal to the sum of its proper divisors
+# The smallest perfect number is 6, which is the sum of 1, 2, and 3. 
+# Other perfect numbers are 28, 496, and 8128.
+
+def isProperDivisor(nr , divisor):
+  ''' Returns True if proper divisor, otherwise False'''
+  return nr % divisor == 0
+
+def isPerfect(nr):
+  '''Returns True if perfect number, otherwise False'''
+  sumDivisors = 1
+  for i in range (2, nr):
+    if isProperDivisor(nr,i):
+      sumDivisors += i
+  return sumDivisors == nr    
+
+
+checkUntil = 50000000 # Check for perfect numbers from 1 to checkUntil
+
+for i in range(2, checkUntil):
+  print("Checking number: ", i , end="\r", flush=True)
+  if isPerfect(i):
+    print("\nThe number", i , " is perfect number")
