aa79f4a089a46c586016bd1fd17ab010c5116d5a
panticzoran <91920971+panticzoran@users.noreply.github.com>
2024-02-01 13:39:06 +0000

    Finalising lists playing

--- main.py
+++ main.py
@@ -14,3 +14,26 @@ print("\n")
 
 for i in range(len(someOtherList)):
   print(someOtherList[i])
+
+print("\n")
+
+students = [("John", ["CompSci", "Physics"]),
+  ("Vusi", ["Maths", "CompSci", "Stats"]),
+  ("Jess", ["CompSci", "Accounting", "Economics", "Management"]),
+  ("Sarah", ["InfSys", "Accounting", "Economics", "CommLaw"]),
+  ("Zuki", ["Sociology", "Economics", "Law", "Stats", "Music"])]
+
+
+# Listing students for the specific condition
+counter = 0
+studentsTakingSubject = ""
+for (studentName, subjectTaken) in students:
+  if "CompSci" in subjectTaken:
+    counter += 1
+    studentsTakingSubject += studentName + " "
+
+print("The total number of students is", len(students))
+print("The number of students taking CompSci is", counter, "and those are:", studentsTakingSubject)
+
+print("\n")
+
