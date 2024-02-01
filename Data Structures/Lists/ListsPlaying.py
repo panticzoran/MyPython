1a03e62349c7ec53a06d4c38aa0e3eb5e1ed565b
panticzoran <91920971+panticzoran@users.noreply.github.com>
2024-02-01 13:39:53 +0000

    Added a new file for playing with lists

---
+++ Data Structures/Lists/ListsPlaying.py
@@ -0,0 +1,39 @@
+# Playing with Lists
+
+someList = [1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1]
+for i in someList:
+  print(i)
+
+print("\n")
+
+someOtherList = ["one", "two", "three", "then", "backwards"]
+for i in someOtherList:
+  print(i)
+
+print("\n")
+
+for i in range(len(someOtherList)):
+  print(someOtherList[i])
+
+print("\n")
+
+students = [("John", ["CompSci", "Physics"]),
+  ("Vusi", ["Maths", "CompSci", "Stats"]),
+  ("Jess", ["CompSci", "Accounting", "Economics", "Management"]),
+  ("Sarah", ["InfSys", "Accounting", "Economics", "CommLaw"]),
+  ("Zuki", ["Sociology", "Economics", "Law", "Stats", "Music"])]
+
+
+# Listing students for the specific condition
+counter = 0
+studentsTakingSubject = ""
+for (studentName, subjectTaken) in students:
+  if "CompSci" in subjectTaken:
+    counter += 1
+    studentsTakingSubject += studentName + " "
+
+print("The total number of students is", len(students))
+print("The number of students taking CompSci is", counter, "and those are:", studentsTakingSubject)
+
+print("\n")
+
