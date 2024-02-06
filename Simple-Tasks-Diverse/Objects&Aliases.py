4fe53dfcae52022726905a3b7c09a4e31b78c4f9
panticzoran <91920971+panticzoran@users.noreply.github.com>
2024-02-06 12:14:58 +0000

    Updating the Objests&Aliases file with info about passing a list as an argument

--- Simple-Tasks-Diverse/Objects&Aliases.py
+++ Simple-Tasks-Diverse/Objects&Aliases.py
@@ -56,3 +56,19 @@ b = a[:]
 print("Lists a and b :", a, b)
 print("a and b are same object:", a is b)
 print("a and b have the same value:", a == b)
+
+
+# Passing a list as an argument actually passes a reference to the list, not a copy or 
+# clone of the list.
+
+def double_stuff(a_list):
+  """ Overwrite each element in a_list with double its value. """
+  for (idx, val) in enumerate(a_list):
+    a_list[idx] = 2 * val
+
+things = [2, 5, 9]
+print("Original list", things)
+double_stuff(things)
+print("List after calling the function", things)
+
+print("\n")
