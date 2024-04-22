b691d97ef6541fc5d95333c09bdbbeba57481bc5
Zoran Pantić <91920971+panticzoran@users.noreply.github.com>
2024-04-22 12:29:49 +0000

    Working version of PureFunctionExample and small correction for FirstClassFunctionExample

--- Functional-Programming/FirstClassFunctionExample.py
+++ Functional-Programming/FirstClassFunctionExample.py
@@ -1,8 +1,8 @@
-# Exploring First-Class Functions, creating a function that accepts another function as
+# Example of First-Class Functions, creating a function that accepts another function as
 # an argument and a list, applying the function to each element in the list
 
-def applyFunction(functionToAppl, listToBeAppliedTo):
-  return [functionToAppl(item) for item in listToBeAppliedTo]
+def applyFunction(functionToApply, listToBeAppliedTo):
+  return [functionToApply(item) for item in listToBeAppliedTo]
 
 # Applying lambda function for square of a number
 listOfNumbers = [1, 2, 3, 4]
